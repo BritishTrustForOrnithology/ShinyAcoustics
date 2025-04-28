844dbfd7ab79188d8b8ad73ae9208ae9595c41f8
Simon Gillings <simon.gillings@bto.org>
2025-04-28 23:05:37 +0100

    fix volume referencing

--- R/run_app.R
+++ R/run_app.R
@@ -5,11 +5,15 @@
 #' @export
 
 
-run_app <- function(vols) {
-
+run_app <- function() {
+  
   #get the drive letters
-  #volumes <<- shinyFiles::getVolumes()()
-  #volumes <<- c("Home" = fs::path_home(), "C" = "C:/")
+  volumes <<- shinyFiles::getVolumes()()
+  volumes <<- c(volumes, "Home" = fs::path_home(), "R Project" = getwd())
+  
+  #register the www path for images and css
   shiny::addResourcePath("www", system.file("www", package = "ShinyAcoustics"))
+  
+  #initialise the app
   shiny::shinyApp(ui = app_ui, server = app_server)
 }
