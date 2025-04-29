e3d2a3cadf11748086d0cde7990a88b56b667d71
Simon Gillings <simon.gillings@bto.org>
2025-04-29 23:00:01 +0100

    update for doc

--- R/nav_button_toggles.R
+++ R/nav_button_toggles.R
@@ -1,6 +1,9 @@
+#' Toggle enable/disable of nav buttons depending on state
+#' 
 #' @description Toggle enable/disable feature of navigation buttons
 #' @param numfiles = the total number of audio files
 #' @param nthfile = which file is currently active
+#' 
 nav_button_toggles <- function(numfiles, nthfile) {
   if(nthfile==1) disable('btn_first') else enable('btn_first')
   if(nthfile==1) disable('btn_previous') else enable('btn_previous')
