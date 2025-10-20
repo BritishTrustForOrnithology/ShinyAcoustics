c445b178f6beaeb130617fcb50dd4b054b79b8b0
Simon Gillings <simon.gillings@bto.org>
2025-10-20 21:53:23 +0100

    option to pass volumes

--- R/run_clip_verifier.R
+++ R/run_clip_verifier.R
@@ -1,31 +1,26 @@
-#' run the main app
+#' run the full verification app
+#' 
 #' @import shiny
 #' 
-#' @param version = 'full' or 'simple'
+#' @param audiodirs = named list providing alternative drives and shortcuts to audio directories
 #' 
 #' @export
 
 
-run_clip_verifier <- function(version = 'simple') {
-  if(!version %in% c('simple','full')) stop('version must be either simple or full')
-  
+run_clip_verifier <- function(audiodirs=NULL) {
+
   #get the drive letters
-  volumes <<- shinyFiles::getVolumes()()
-  volumes <<- c(volumes, "Home" = fs::path_home(), "R Project" = getwd())
+  if(!is.null(audiodirs)) volumes <<- audiodirs
+  if(is.null(audiodirs)) {
+    volumes <<- shinyFiles::getVolumes()()
+    volumes <<- c(volumes, "Home" = fs::path_home(), "R Project" = getwd())
+  }
   
   #register the www path for images and css
   shiny::addResourcePath("www", 
                          system.file("www", package = "ShinyAcoustics"))
   
-  #initialise the app
-  if(version == 'simple') {
-    shiny::shinyApp(ui = app_verifier_simple_ui, 
-                    server = app_verifier_simple_server, 
-                    options = list(launch.browser=TRUE))
-  }
-  if(version == 'full') {
-    shiny::shinyApp(ui = app_verifier_full_ui, 
-                    server = app_verifier_full_server, 
-                    options = list(launch.browser=TRUE))
-  }
+  shiny::shinyApp(ui = app_verifier_full_ui, 
+                  server = app_verifier_full_server, 
+                  options = list(launch.browser=TRUE))
 }
