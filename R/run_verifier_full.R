bd6e277633dd5b463bf8c68fe5f61956a7d147d3
Simon Gillings <simon.gillings@bto.org>
2025-10-20 20:24:17 +0100

    modify to allow different versions to be run

--- R/run_verifier_full.R
+++ R/run_verifier_full.R
@@ -1,10 +1,13 @@
 #' run the main app
 #' @import shiny
 #' 
+#' @param version = 'full' or 'simple'
+#' 
 #' @export
 
 
-run_clip_verifier <- function() {
+run_clip_verifier <- function(version = 'simple') {
+  if(!version %in% c('simple','full')) stop('version must be either simple or full')
   
   #get the drive letters
   volumes <<- shinyFiles::getVolumes()()
@@ -15,7 +18,14 @@ run_clip_verifier <- function() {
                          system.file("www", package = "ShinyAcoustics"))
   
   #initialise the app
-  shiny::shinyApp(ui = app_verifier_full_ui, 
-                  server = app_verifier_full_server, 
-                  options = list(launch.browser=TRUE))
+  if(version == 'simple') {
+    shiny::shinyApp(ui = app_verifier_simple_ui, 
+                    server = app_verifier_simple_server, 
+                    options = list(launch.browser=TRUE))
+  }
+  if(version == 'full') {
+    shiny::shinyApp(ui = app_verifier_full_ui, 
+                    server = app_verifier_full_server, 
+                    options = list(launch.browser=TRUE))
+  }
 }
